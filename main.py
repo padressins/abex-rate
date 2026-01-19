c5a69b36f1fc18d91961835e182abd066d2bcfb8
padressins <uiapetrakevich@gmail.com>
2026-01-19 19:18:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def calculate():
         amount = float(data['amount'])
         
         rate = get_btc_rate()
-        sum_moment = amount * rate * 1.2
-        sum_delay = amount * rate * 1.13
+        sum_moment = amount * rate * 1.22
+        sum_delay = amount * rate * 1.18
 
         return jsonify({
             "sum_moment": round(sum_moment),
@@ -125,5 +125,6 @@ def set_rate():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
 
